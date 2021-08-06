f8c3ade496a3f225084a740a76cb48c3aed568f6
allendlg <allen.delagarza01@gmail.com>
2021-08-06 10:15:56 -0500

    Completed Project1

--- index.js
+++ index.js
@@ -4,10 +4,52 @@ const fox_result = document.getElementById('fox_result');
 const cat_result = document.getElementById('cat_result');
 let mouseCursor = document.querySelector(".cursor");
 
+fox_btn.addEventListener("mouseenter", function(event) {
+	// highlight the mouseenter target
+	event.target.style.color = "purple";
+  
+	// reset the color after a short delay
+	setTimeout(function() {
+	  event.target.style.color = "";
+	}, 500);
+  }, false);
+  
+  // This handler will be executed every time the cursor
+  // is moved over a different list item
+  cat_btn.addEventListener("mouseover", function( event ) {
+	// highlight the mouseover target
+	event.target.style.color = "orange";
+  
+	// reset the color after a short delay
+	setTimeout(function() {
+	  event.target.style.color = "";
+	}, 500);
+  }, false);
+
+  cat_btn.addEventListener("mouseenter", function(event) {
+	// highlight the mouseenter target
+	event.target.style.color = "purple";
+  
+	// reset the color after a short delay
+	setTimeout(function() {
+	  event.target.style.color = "";
+	}, 500);
+  }, false);
+  
+  // This handler will be executed every time the cursor
+  // is moved over a different list item
+  fox_btn.addEventListener("mouseover", function( event ) {
+	// highlight the mouseover target
+	event.target.style.color = "orange";
+  
+	// reset the color after a short delay
+	setTimeout(function() {
+	  event.target.style.color = "";
+	}, 500);
+  }, false);
 
 fox_btn.addEventListener('click', getRandomFox);
 cat_btn.addEventListener('click', getRandomCat);
-fox_btn.addEventListener('click', getRandomFox)
 
 function getRandomFox() {
 	fetch('https://randomfox.ca/floof/')
@@ -18,13 +60,12 @@ function getRandomFox() {
 }
 
 function getRandomCat() {
-	fetch('https://aws.random.cat/meow' )
+	fetch('https://random.dog/woof.json')
 		.then(res => res.json())
 		.then(data => {
-
-				cat_result.innerHTML = `<img src=${data.file} alt="cat" />`;
-			})
-		};
+			cat_result.innerHTML = `<img src=${data.url} alt="cat" />`
+		});
+}
 
  //Grabing classes
 let cursor = document.querySelector(".cursor");
